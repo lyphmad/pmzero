c6646a482c990c00b9c2a9642ea5f82367aaa9e0
박현승 <hyeon9104@unist.ac.kr>
2018-02-22 23:45:58 +0900

    Github opens in new tab/window

--- edit.php
+++ edit.php
@@ -10,96 +10,96 @@
       <a href="score_this_month.php" class="w3-bar-item w3-button">이번 달 기록</a>
       <a href="record.html" class="w3-bar-item w3-button">기록 입력</a>
       <a href="new_member.html" class="w3-bar-item w3-button">사용자 등록</a>
-      <a href="https://github.com/lyphmad/pmzero" class="w3-bar-item w3-button"> GitHub </a>
+      <a href="https://github.com/lyphmad/pmzero" class="w3-bar-item w3-button" target="_blank" style="margin-top:100px;"> GitHub </a>
     </div>
 
     <div class="w3-main" style="margin-left:200px">
       <div style="background-color: #001c54; color: white">
         <button class="w3-button w3-xlarge w3-hide-large" onclick="w3_open()">&#9776;</button>
-        <div class="w3-container" style="background-color: #001c54;">
+        <div class="w3-container">
           <h1>기록 수정</h1>
         </div>
       </div>
+
+      <?php
+      // Create connection
+      $conn = new mysqli("localhost", "ubuntu", "", "pmzero");
+      // Check connection
+      if ($conn->connect_error) {
+          die("Connection failed: " . $conn->connect_error);
+      }
+
+      $conn->set_charset("utf8");
+
+      $gameID = $_GET["id"];
+
+      $results = $conn->query("SELECT * FROM Cache WHERE gameID = $gameID;");
+      $rowitem = $results->fetch_array();
+      $conn->close();
+      ?>
+
+      <form method="get" action="record_edit.php">
+        <input name="gameID" type="text" value="<?=$gameID?>" hidden>
+
+        <div class="w3-container w3-row-padding">
+          <div class="w3-col"><h3>1위</h3></div>
+          <div class="w3-quarter">
+            <input name="eastName" class="w3-input" type="text" value="<?=$rowitem['eastName']?>" required>
+          </div>
+          <div class="w3-quarter">
+            <input id="eastScore" name="eastScore" class="w3-input" type="text" onblur="getTotal()" value="<?=$rowitem['eastScore']?>">
+          </div>
+        </div>
+
+        <div class="w3-container w3-row-padding">
+          <div class="w3-col"><h3>남</h3></div>
+          <div class="w3-quarter">
+            <input name="southName" class="w3-input" type="text" value="<?=$rowitem['southName']?>" required>
+          </div>
+          <div class="w3-quarter">
+            <input id="southScore" name="southScore" class="w3-input" type="text" onblur="getTotal()" value="<?=$rowitem['southScore']?>">
+          </div>
+        </div>
+
+        <div class="w3-container w3-row-padding">
+          <div class="w3-col"><h3>서</h3></div>
+          <div class="w3-quarter">
+            <input name="westName" class="w3-input" type="text" value="<?=$rowitem['westName']?>" required>
+          </div>
+          <div class="w3-quarter">
+            <input id="westScore" name="westScore" class="w3-input" type="text" onblur="getTotal()" value="<?=$rowitem['westScore']?>">
+          </div>
+        </div>
+
+        <div class="w3-container w3-row-padding">
+          <div class="w3-col"><h3>북</h3></div>
+          <div class="w3-quarter">
+            <input name="northName" class="w3-input" type="text" value="<?=$rowitem['northName']?>" required>
+          </div>
+          <div class="w3-quarter">
+            <input id="northScore" name="northScore" class="w3-input" type="text" onblur="getTotal()" value="<?=$rowitem['northScore']?>">
+          </div>
+        </div>
+
+        <div class="w3-container w3-row-padding">
+          <div class="w3-col"><h3>공탁</h3></div>
+          <div class="w3-quarter">
+            <input id="leftover" name="leftover" class="w3-input" type="text" onblur="getTotal()" value="<?=$rowitem['leftover']?>">
+          </div>
+        </div>
+
+        <div class="w3-container w3-row-padding">
+          <div class="w3-col"><h3>합계</h3></div>
+          <div class="w3-quarter">
+            <input id="total" name="total" class="w3-input" type="text" disabled value="0">
+          </div>
+        </div>
+
+        <div class="w3-container w3-row-padding" style="margin-top: 10px; margin-left: 8px;">
+          <input id="submit" class="w3-button w3-border" type="submit" value="기록 입력">
+        </div>      
+      </form>
     </div>
-
-    <?php
-    // Create connection
-    $conn = new mysqli("localhost", "ubuntu", "", "pmzero");
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-
-    $conn->set_charset("utf8");
-
-    $gameID = $_GET["id"];
-
-    $results = $conn->query("SELECT * FROM Cache WHERE gameID = $gameID;");
-    $rowitem = $results->fetch_array();
-    $conn->close();
-    ?>
-
-    <form method="get" action="record_edit.php">
-      <input name="gameID" type="text" value="<?=$gameID?>" hidden>
-
-      <div class="w3-container w3-row-padding">
-        <div class="w3-col"><h3>1위</h3></div>
-        <div class="w3-quarter">
-          <input name="eastName" class="w3-input" type="text" value="<?=$rowitem['eastName']?>" required>
-        </div>
-        <div class="w3-quarter">
-          <input id="eastScore" name="eastScore" class="w3-input" type="text" onblur="getTotal()" value="<?=$rowitem['eastScore']?>">
-        </div>
-      </div>
-
-      <div class="w3-container w3-row-padding">
-        <div class="w3-col"><h3>남</h3></div>
-        <div class="w3-quarter">
-          <input name="southName" class="w3-input" type="text" value="<?=$rowitem['southName']?>" required>
-        </div>
-        <div class="w3-quarter">
-          <input id="southScore" name="southScore" class="w3-input" type="text" onblur="getTotal()" value="<?=$rowitem['southScore']?>">
-        </div>
-      </div>
-
-      <div class="w3-container w3-row-padding">
-        <div class="w3-col"><h3>서</h3></div>
-        <div class="w3-quarter">
-          <input name="westName" class="w3-input" type="text" value="<?=$rowitem['westName']?>" required>
-        </div>
-        <div class="w3-quarter">
-          <input id="westScore" name="westScore" class="w3-input" type="text" onblur="getTotal()" value="<?=$rowitem['westScore']?>">
-        </div>
-      </div>
-
-      <div class="w3-container w3-row-padding">
-        <div class="w3-col"><h3>북</h3></div>
-        <div class="w3-quarter">
-          <input name="northName" class="w3-input" type="text" value="<?=$rowitem['northName']?>" required>
-        </div>
-        <div class="w3-quarter">
-          <input id="northScore" name="northScore" class="w3-input" type="text" onblur="getTotal()" value="<?=$rowitem['northScore']?>">
-        </div>
-      </div>
-
-      <div class="w3-container w3-row-padding">
-        <div class="w3-col"><h3>공탁</h3></div>
-        <div class="w3-quarter">
-          <input id="leftover" name="leftover" class="w3-input" type="text" onblur="getTotal()" value="<?=$rowitem['leftover']?>">
-        </div>
-      </div>
-
-      <div class="w3-container w3-row-padding">
-        <div class="w3-col"><h3>합계</h3></div>
-        <div class="w3-quarter">
-          <input id="total" name="total" class="w3-input" type="text" disabled value="0">
-        </div>
-      </div>
-
-      <div class="w3-container w3-row-padding" style="margin-top: 10px; margin-left: 8px;">
-        <input id="submit" class="w3-button w3-border" type="submit" value="기록 입력">
-      </div>      
-    </form>
 
     <script>
       function w3_open() {
