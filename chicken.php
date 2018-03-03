991e844b02bef947b22832b871aea6c90ab565eb
박현승 <hyeon9104@unist.ac.kr>
2018-03-03 20:46:25 +0900

    파일 업로드 수정

--- chicken.php
+++ chicken.php
@@ -6,13 +6,22 @@ if(isset($_FILES["photo"])) {
     $fileCount = count($_FILES["photo"]["name"]);
 
     for ($i=0; $i < $fileCount; $i++) {        
-        $ext = pathinfo($_FILES["photo"]["name"], PATHINFO_EXTENSION);
-        $target_file = $target_dir . "chicken" . $gameID . sprintf("-%04d.", $i) . $ext;
+        $ext = pathinfo($_FILES["photo"]["name"][$i], PATHINFO_EXTENSION);
+        $target_file = $target_dir . "chicken" . sprintf("%04d", $gameID) . sprintf("-%04d.", $i) . $ext;
+        echo $_FILES["photo"]["tmp_name"][$i]."\n";
 
         if($check === false) {
             exit("File is not an image.");
         } else {
-            move_uploaded_file($_FILES["photo"]["tmp_name"], $target_file);
+            if(move_uploaded_file($_FILES["photo"]["tmp_name"][$i], $target_file)) {
+                echo "<script>
+                alert('등록되었습니다.');    
+                window.location.href='score_this_month.php';
+                </script>";
+            }
+            else {
+                exit("File not uploaded.");
+            }
         }
     }
 }
@@ -26,11 +35,11 @@ if ($conn->connect_error) {
 
 $conn->set_charset("utf8");
 
-if (empty($_POST['bought'])) {
-	$bought = 0;
+if ($_POST['bought'] === "on") {
+	$bought = 1;
 }
 else {
-	$bought = $_POST['bought'];
+	$bought = 0;
 }
 
 $query = "INSERT INTO Chicken SET
