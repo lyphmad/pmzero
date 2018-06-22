e62a358b19ec7191de900d0257dcf5a10f126fa0
박현승 <lyphmad@gmail.com>
2018-06-22 11:25:53 +0900

    수정 기능 수정 (pun intended)

--- edit_form.php
+++ edit_form.php
@@ -1,42 +1,41 @@
+<?php
+$gameID = $_GET["id"];
+// Create connection
+$conn = new mysqli("localhost", "openvpnas", "", "pmzero");
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$conn->set_charset("utf8");
+
+$result = $conn->query("SELECT * FROM Members ORDER BY `name`;");
+$members = array();
+while ($rowitem = $result->fetch_array()) {
+  $members[$rowitem['memberID']] = $rowitem['name'];
+}
+
+$results = $conn->query("SELECT * FROM Games WHERE gameID = $gameID;");
+$rowitem = $results->fetch_array();
+$conn->close();
+?>
+
 <!DOCTYPE html>
 <html>
 	<title>UNIST 마작 소모임 ±0</title>
 	<meta name="viewport" charset="utf-8" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="w3.css">
 
-	<body onload="getTotal()">    
+	<body>    
 		<div w3-include-html="menubar.html"></div>
 
 		<div class="w3-main" style="margin-left:200px">
-			<div class="w3-card" style="background-color: #001c54; color: white">
+			<div class="w3-card" style="background-color: #001c54; color: white;">
 				<button class="w3-button w3-xlarge w3-hide-large" onclick="w3_open()">&#9776;</button>
-				<div class="w3-container">
-					<h1>기록 수정</h1>
+				<div class="w3-container" style="margin-left:10px;">
+					<h1>기록 입력</h1>
 				</div>
 			</div>
-
-			<?php
-			$gameID = $_GET["id"];
-
-			// Create connection
-			$conn = new mysqli("localhost", "openvpnas", "", "pmzero");
-			// Check connection
-			if ($conn->connect_error) {
-					die("Connection failed: " . $conn->connect_error);
-			}
-
-			$conn->set_charset("utf8");
-
-			$result = $conn->query("SELECT * FROM Members ORDER BY `name`;");
-			$names = array();
-			while ($rowitem = $result->fetch_array()) {
-				$names[$rowitem['memberID']] = $rowitem['name'];
-			}
-
-			$results = $conn->query("SELECT * FROM Games WHERE gameID = $gameID;");
-			$rowitem = $results->fetch_array();
-			$conn->close();
-			?>
 
 			<form method="get" action="edit.php" onsubmit="return validate()">
 				<input name="gameID" type="text" value="<?=$gameID?>" hidden>
@@ -44,15 +43,15 @@
 				<div class="row">
 					<div class="w3-container w3-cell wind"><h3>동</h3></div>
 					<div class="w3-container w3-cell" style="width:130px;">
-						<select id="eastName" name="eastName" style="width:100%;" autocomplete="off" required>
+						<select id="eastID" name="eastID" style="width:100%;" autocomplete="off" required>
 							<option disabled>이름</option>
 							<?php
-							foreach ($names as $id => $name) {
+							foreach ($members as $id => $name) {
 								if ($id == $rowitem['eastID']) {
-									echo '<option value="'.$name.'" selected>'.$name.'</option>';
+									echo '<option value="'.$id.'" selected>'.$name.'</option>';
 								}
 								else {
-									echo '<option value="'.$name.'">'.$name.'</option>';
+									echo '<option value="'.$id.'">'.$name.'</option>';
 								}
 							}
 							?>
@@ -66,14 +65,14 @@
 				<div class="row">
 					<div class="w3-container w3-cell wind"><h3>남</h3></div>
 					<div class="w3-container w3-cell" style="width:130px;">
-						<select id="southName" name="southName" style="width:100%;" autocomplete="off" required>
+						<select id="southID" name="southID" style="width:100%;" autocomplete="off" required>
 							<?php
-							foreach ($names as $id => $name) {
+							foreach ($members as $id => $name) {
 								if ($id == $rowitem['southID']) {
-									echo '<option value="'.$name.'" selected>'.$name.'</option>';
+									echo '<option value="'.$id.'" selected>'.$name.'</option>';
 								}
 								else {
-									echo '<option value="'.$name.'">'.$name.'</option>';
+									echo '<option value="'.$id.'">'.$name.'</option>';
 								}
 							}
 							?>
@@ -87,14 +86,14 @@
 				<div class="row">
 					<div class="w3-container w3-cell wind"><h3>서</h3></div>
 					<div class="w3-container w3-cell" style="width:130px;">
-						<select id="westName" name="westName" style="width:100%;" autocomplete="off" required>
+						<select id="westID" name="westID" style="width:100%;" autocomplete="off" required>
 							<?php
-							foreach ($names as $id => $name) {
+							foreach ($members as $id => $name) {
 								if ($id == $rowitem['westID']) {
-									echo '<option value="'.$name.'" selected>'.$name.'</option>';
+									echo '<option value="'.$id.'" selected>'.$name.'</option>';
 								}
 								else {
-									echo '<option value="'.$name.'">'.$name.'</option>';
+									echo '<option value="'.$id.'">'.$name.'</option>';
 								}
 							}
 							?>
@@ -108,14 +107,14 @@
 				<div class="row">
 					<div class="w3-container w3-cell wind"><h3>북</h3></div>
 					<div class="w3-container w3-cell" style="width:130px;">
-						<select id="northName" name="northName" style="width:100%;" autocomplete="off" required>
+						<select id="northID" name="northID" style="width:100%;" autocomplete="off" required>
 							<?php
-							foreach ($names as $id => $name) {
+							foreach ($members as $id => $name) {
 								if ($id == $rowitem['northID']) {
-									echo '<option value="'.$name.'" selected>'.$name.'</option>';
+									echo '<option value="'.$id.'" selected>'.$name.'</option>';
 								}
 								else {
-									echo '<option value="'.$name.'">'.$name.'</option>';
+									echo '<option value="'.$id.'">'.$name.'</option>';
 								}
 							}
 							?>
@@ -149,71 +148,71 @@
 		</div>
 
 		<style>			
-			.w3-container {
-				vertical-align: middle;
-			}
+		.w3-container {
+			vertical-align: middle;
+		}
 
-			.row {
-				padding-top: 20px;
-				padding-left: 20px;
-				padding-right: 20px;
-			}
-			
-			.wind {
-				padding-right: 50px;
-			}
+		.row {
+			padding-top: 20px;
+			padding-left: 20px;
+			padding-right: 20px;
+		}
+
+		.wind {
+			padding-right: 50px;
+		}
 		</style>
 
 		<script src="https://www.w3schools.com/lib/w3.js"></script> 
 		<script>
-			w3.includeHTML();
-			function w3_open() {
-					document.getElementById("mySidebar").style.display = "block";
-			}
+		w3.includeHTML();
+		function w3_open() {
+				document.getElementById("mySidebar").style.display = "block";
+		}
 
-			function w3_close() {
-					document.getElementById("mySidebar").style.display = "none";
-			}
+		function w3_close() {
+				document.getElementById("mySidebar").style.display = "none";
+		}
 
-			function getTotal() {
-				var east = parseInt(document.getElementById('eastScore').value);
-				var south = parseInt(document.getElementById('southScore').value);
-				var west = parseInt(document.getElementById('westScore').value);
-				var north = parseInt(document.getElementById('northScore').value);
-				var left = parseInt(document.getElementById('leftover').value);
+		function getTotal() {
+			var east = parseInt(document.getElementById('eastScore').value);
+			var south = parseInt(document.getElementById('southScore').value);
+			var west = parseInt(document.getElementById('westScore').value);
+			var north = parseInt(document.getElementById('northScore').value);
+			var left = parseInt(document.getElementById('leftover').value);
 
-				var tot = 0;
-				if (east) { tot += east; }
-				if (south) { tot += south; }
-				if (west) { tot += west; }
-				if (north) { tot += north; }
-				if (left) { tot += left; }
+			var tot = 0;
+			if (east) { tot += east; }
+			if (south) { tot += south; }
+			if (west) { tot += west; }
+			if (north) { tot += north; }
+			if (left) { tot += left; }
+			document.getElementById('total').value = tot;
+
+			if (tot == 100000) {
+				document.getElementById("submit").disabled = false;
 				document.getElementById('total').value = tot;
-
-				if (tot == 100000) {
-					document.getElementById("submit").disabled = false;
-					document.getElementById('total').value = tot;
+			}
+			else {
+				var diff = tot - 100000;
+				if (diff > 0) {
+					diff = "+" + diff;
 				}
-				else {
-					var diff = tot - 100000;
-					if (diff > 0) {
-						diff = "+" + diff;
-					}
-					var minus = "(" + diff + ")";
-					var str = tot + " " + minus.fontcolor ("red");
-					document.getElementById('total').innerHTML = str;
-					document.getElementById("submit").disabled = true;
-				}
-
-				return tot;
+				var minus = "(" + diff + ")";
+				var str = tot + " " + minus.fontcolor ("red");
+				document.getElementById('total').innerHTML = str;
+				document.getElementById("submit").disabled = true;
 			}
 
-			function validate() {
-				if (getTotal() != 100000) {
-					alert("점수 합계를 확인하세요");
-					return false;
-				}
+			return tot;
+		}
+
+		function validate() {
+			if (getTotal() != 100000) {
+				alert("점수 합계를 확인하세요");
+				return false;
 			}
+		}
 		</script>
 	</body>
 </html>
